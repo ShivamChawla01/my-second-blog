45fa23ad794e9280ce6af78ce643a44bd370db03
Shivam.Chawla01 <ch.shivam1@gmail.com>
2019-11-27 00:25:00 +0530

    fifth commit that deals with forms and authentication

--- tictactoe/player/models.py
+++ tictactoe/player/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Invitation(models.Model):
+    from_user = models.ForeignKey(User, related_name="invitation_sent", on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name="invitation_received", verbose_name="User to invite",
+                                help_text="Please select a user you want to play a game with.", on_delete=models.CASCADE)
+
+    message = models.CharField(max_length=300, blank=True, verbose_name="Optional Message",
+                               help_text="It's always nice to add a friendly message!")
+
+
+
